209c5332c956212ee11fae1e7eb01d02c00b9041
Eric Lee <Raidenlkl@gmail.com>
2019-08-17 23:31:45 +1000

    Server app can host appropriately

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 5000;
+
+app.use(express.json());
+app.use(express.static('./client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.get('/demo', (req, res) => {
   res.json({ msg: 'Hello World' });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on Port ${PORT}`));
